9668d2ab2195536e2e4d751007e9a6c663ac5629
Thomas McNiven <hello@vevix.net>
2019-08-15 21:21:41 +1000

    add build step and github actions

---
+++ build.js
@@ -0,0 +1,33 @@
+const JSZip = require('node-zip');
+const fs = require('fs');
+
+const FILES = [
+  'manifest.json',
+  'main.js',
+  'overlay.js',
+  'style.css',
+  'resources/icon-16.jpg',
+  'resources/icon-48.jpg',
+  'resources/icon-96.jpg',
+  'resources/icon-128.jpg'
+];
+
+function run() {
+  try {
+    console.log('🔥 starting build...');
+    if (!fs.existsSync('build')) {
+      fs.mkdirSync('build');
+    }
+    const zip = new JSZip();
+    for (const file of FILES) {
+      zip.file(file, fs.readFileSync(file));
+    }
+    const data = zip.generate({ type: 'nodebuffer' });
+    fs.writeFileSync('build/twitch-chat-overlay.xpi', data);
+    console.log('🚀 build finished');
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+run();
